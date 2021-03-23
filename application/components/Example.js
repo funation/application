8c184cd2ad2d11beef5e569c308fa8e76876ee90
Songminseon <smsun0329@naver.com>
2021-03-23 19:00:47 +0900

    add ReadMe

---
+++ application/components/Example.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+
+const Example = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default Example;
