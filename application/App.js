bed4f7a7197576b0e62b74086a322805f69671f8
Songminseon <smsun0329@naver.com>
2021-04-09 14:14:11 +0900

    commit for check npm

--- application/App.js
+++ application/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import ExampleForKakao from "./components/ExampleForKakao";
+import styled from "styled-components/native";
+
+const AppWrapper = styled.View`
+  width:100%;
+  height:100%;
+  background:black;
+`;
 
 const App = () => {
   return(
-    <>
+    <AppWrapper>
       <ExampleForKakao />
-    </>
+    </AppWrapper>
   );
 };
 
